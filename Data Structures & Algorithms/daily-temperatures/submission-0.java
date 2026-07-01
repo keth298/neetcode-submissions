88abbb1b115bc9eb31acc262952a0c3a39e0474a
keth298 <kimethan64@gmail.com>
2026-06-30 23:17:36 -0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] res = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
