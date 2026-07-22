26046a86e10229b0e8ebef972649181effb830c4
keth298 <kimethan64@gmail.com>
2026-07-22 11:46:07 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 2) return n;
+        int[] cache = new int[n];
+        cache[0] = 1;
+        cache[1] = 2;
+        for(int i = 2; i < n; i++){
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        return cache[n - 1];
+    }
+}
